6e6c66a4b58cb882c86349c204f53da53e4c9722
Daniel-Ibarrola <dis96051@gmail.com>
2023-02-14 10:00:07 -0600

    Server sends new data when it arrives

--- src/clientserver/tcp/server.py
+++ src/clientserver/tcp/server.py
@@ -1,3 +1,4 @@
+import time
 from collections import deque
 import socketserver
 import threading
@@ -8,14 +9,33 @@ class TCPRequestHandler(socketserver.BaseRequestHandler):
     """ Handler for an individual client.
     """
 
+    @staticmethod
+    def send_and_receive_confirmation(socket, data):
+        socket_send(socket, data, TCPServer.MSG_LEN)
+        # Client sends a notification that the whole message has been received
+        return socket_receive(socket, TCPServer.MSG_LEN)
+
     def handle(self) -> None:
         """ Send all data from the queue to the client.
         """
+        self.handle_new_client()
+
+        # Send the newest data
+        prev = TCPServer.latest
+        while not TCPServer.STOP:
+            if TCPServer.latest > prev:
+                try:
+                    self.send_and_receive_confirmation(self.request, TCPServer.buffer[-1])
+                except ConnectionError:
+                    break
+                prev = TCPServer.latest
+
+    def handle_new_client(self) -> None:
+        """ If a new client connects all data in the queue is sent.
+        """
         for data in TCPServer.buffer:
             try:
-                socket_send(self.request, data, TCPServer.MSG_LEN)
-                # Client sends a notification that the whole message has been received
-                msg = socket_receive(self.request, TCPServer.MSG_LEN)
+                msg = self.send_and_receive_confirmation(self.request, data)
                 if msg.decode(encoding="utf-8") != "RECVD\r\n":
                     break
             except ConnectionError:
@@ -27,21 +47,34 @@ class TCPServer:
     """
     buffer = None  # type: deque[bytes]
     MSG_LEN = 2048
+    STOP = False
+    latest = None
 
     def __init__(self, ip: str, port: int, buff_size: int = 100):
+        self._setup_server(buff_size)
         self._ip = ip
         self._port = port
-
-        TCPServer.buffer = deque(maxlen=buff_size)
 
         self._server = socketserver.ThreadingTCPServer(
             (self._ip, self._port), TCPRequestHandler
         )
+        self._server.allow_reuse_address = True
 
-    @staticmethod
-    def put(data: bytes) -> None:
-        """ Put data in the queue. """
-        TCPServer.buffer.append(data)
+    @property
+    def port(self) -> int:
+        return self._port
+
+    @classmethod
+    def put(cls, data: bytes) -> None:
+        """ Put data in the buffer. """
+        cls.latest = time.time()
+        cls.buffer.append(data)
+
+    @classmethod
+    def _setup_server(cls, buff_size):
+        cls.buffer = deque(maxlen=buff_size)
+        cls.STOP = False
+        cls.latest = time.time()
 
     def run(self):
         """ Starts the thread with the server. """
@@ -50,6 +83,7 @@ class TCPServer:
         server_thread.start()
 
     def shutdown(self):
+        TCPServer.STOP = True
         self._server.shutdown()
 
     def __enter__(self):
--- tests/test_tcp/test_server.py
+++ tests/test_tcp/test_server.py
@@ -1,52 +1,102 @@
 import socket
 import threading
+import time
+import pytest
 
 from clientserver.tcp.server import TCPServer, socket_receive, socket_send
 
 
-responses = [[] for ii in range(3)]
+class FakeClient:
 
+    def __init__(self, ip, port, msg_len):
+        self.ip = ip
+        self.port = port
+        self.msg_len = msg_len
 
-def client(ip, port, msg_len, index):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((ip, port))
+        self.socket = None
+        self.stop = False
 
-        for _ in range(3):
-            data = socket_receive(sock, msg_len)
-            responses[index].append(data)
+        self.thread = None
 
+        self.responses = []
+
+    def start(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.ip, self.port))
+
+        self.thread = threading.Thread(target=self.receive, daemon=True)
+        self.thread.start()
+
+    def receive(self):
+        while not self.stop:
+            try:
+                data = socket_receive(self.socket, self.msg_len)
+            except ConnectionError:
+                time.sleep(0.1)
+                continue
+            self.responses.append(data)
             # Confirm that the message arrived
-            socket_send(sock, b"RECVD\r\n", msg_len)
+            socket_send(self.socket, b"RECVD\r\n", self.msg_len)
+
+    def shutdown(self):
+        self.stop = True
+        self.thread.join()
+        self.socket.close()
+
+
+def initialize_server(ip, port):
+    start = time.time()
+
+    while time.time() - start < 60:
+        try:
+            return TCPServer(ip, port, buff_size=5)
+        except OSError:
+            port += 1
+
+    pytest.fail("Failed to start server")
+
+
+def get_server_and_clients(port=2000, n_clients=3):
+    ip, port = "localhost", port
+
+    server = initialize_server(ip, port)
+    TCPServer.MSG_LEN = 7
+
+    clients = [FakeClient(ip, server.port, server.MSG_LEN) for _ in range(n_clients)]
+
+    return server, clients
+
+
+def start_clients(clients):
+    for cl in clients:
+        cl.start()
+
+
+def stop_clients(clients):
+    for cl in clients:
+        cl.shutdown()
 
 
 def test_server_accepts_and_sends_data_to_multiple_clients():
-    ip, port = "localhost", 12341
-    server = TCPServer(ip, port, buff_size=5)
-
-    msg_len = 7
-    TCPServer.MSG_LEN = msg_len
+    server, clients = get_server_and_clients()
 
     server.put(b"Msg 1\r\n")
     server.put(b"Msg 2\r\n")
     server.put(b"Msg 3\r\n")
 
-    threads = []
     with server:
         server.run()
+        start_clients(clients)
 
-        for ii in range(3):
-            threads.append(threading.Thread(target=client, args=(ip, port, msg_len, ii)))
-            threads[ii].start()
-
-        for th in threads:
-            th.join()
+        time.sleep(0.1)  # wait for all messages to be received
 
         server.shutdown()
+        stop_clients(clients)
 
-    assert all(len(responses[ii]) == 3 for ii in range(3))
-    assert all(responses[jj][0] == b"Msg 1\r\n" for jj in range(3))
-    assert all(responses[jj][1] == b"Msg 2\r\n" for jj in range(3))
-    assert all(responses[jj][2] == b"Msg 3\r\n" for jj in range(3))
+    expected = [b"Msg 1\r\n", b"Msg 2\r\n", b"Msg 3\r\n"]
+    assert clients[0].responses == expected
+    assert clients[1].responses == expected
+    assert clients[2].responses == expected
 
 
 def test_put_server_buff_size_exceeded():
@@ -63,3 +113,24 @@ def test_put_server_buff_size_exceeded():
 
     assert server.buffer[0].decode() == "second"
     assert server.buffer[1].decode() == "third"
+
+
+def test_server_sends_when_new_data_arrives():
+    server, clients = get_server_and_clients(port=1233, n_clients=2)
+
+    with server:
+        server.run()
+        start_clients(clients)
+
+        server.put(b"Msg 1\r\n")
+        time.sleep(0.2)
+
+        server.put(b"Msg 2\r\n")
+        time.sleep(0.2)
+
+        server.shutdown()
+        stop_clients(clients)
+
+    expected = [b"Msg 1\r\n", b"Msg 2\r\n"]
+    assert clients[0].responses == expected
+    assert clients[1].responses == expected
